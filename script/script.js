3bc2d99ecb05e2143b68e8e2a0cf2235ff3683ce
Sergey Meshchankin <smeshchankin@gmail.com>
2020-05-15 23:44:51 +0300

    day 1. refactoring

--- script/script.js
+++ script/script.js
@@ -1,64 +1,95 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const btnOpenModal = document.getElementById('btnOpenModal');
-    const modalBlock = document.getElementById('modalBlock');
-    const closeModal = document.getElementById('closeModal');
-    const questionTitle = document.getElementById('question');
-    const formAnswers = document.getElementById('formAnswers');
-    const menuButton = document.getElementById('burger');
-    const modalWrap = document.querySelector('.modal');
-
-    function handleWidth() {
-        let clientWidth = document.documentElement.clientWidth;
-        menuButton.style.display = clientWidth > 768 ? 'none' : 'flex';
-    }
-
-    function showDialog() {
-        menuButton.classList.add('active');
-        modalBlock.classList.add('d-block');
-        playTest();
-    }
-
-    function hideDialog() {
-        menuButton.classList.remove('active');
-        modalBlock.classList.remove('d-block');
-    }
-
-    window.addEventListener('resize', handleWidth);
-    menuButton.addEventListener('click', showDialog);
-    btnOpenModal.addEventListener('click', showDialog);
-    closeModal.addEventListener('click', hideDialog);
-    modalWrap.addEventListener('click', function(event) {
-        if (event.target.id === 'modalBlock') {
-            hideDialog();
+    let elems = {
+        button: {
+            openModal: '#btnOpenModal',
+            menu: '#burger'
+        },
+        modal: {
+            id: '#modalBlock',
+            close: '#closeModal',
+            question: '#question',
+            answers: '#formAnswers'
         }
-    });
-    handleWidth();
+    };
+    elems = applySelector(elems);
+
+    const handle = {
+        window: {
+            width: () => {
+                let clientWidth = document.documentElement.clientWidth;
+                elems.button.menu.style.display = clientWidth > 768 ? 'none' : 'flex';
+            }
+        },
+        dialog: {
+            show: () => {
+                elems.button.menu.classList.add('active');
+                elems.modal.id.classList.add('d-block');
+                playTest();
+            },
+            hide: () => {
+                elems.button.menu.classList.remove('active');
+                elems.modal.id.classList.remove('d-block');
+            },
+            outside: (event) => {
+                if (event.target === elems.modal.id) {
+                    handle.dialog.hide();
+                }
+            }
+        }
+    }
+
+    function init() {
+        window.addEventListener('resize', handle.window.width);
+        elems.button.menu.addEventListener('click', handle.dialog.show);
+        elems.button.openModal.addEventListener('click', handle.dialog.show);
+        elems.modal.close.addEventListener('click', handle.dialog.hide);
+        elems.modal.id.addEventListener('click', handle.dialog.outside);
+
+        handle.window.width();
+    }
 
     const playTest = () => {
         const renderQuestions = () => {
-            questionTitle.textContent = 'Какого цвета бургер вы хотите?';
+            elems.modal.question.textContent = 'Какого цвета бургер вы хотите?';
+
             const type = 'radio';
             const data = [
                 { id: 'answerItem1', text: 'Стандарт', img: './image/burger.png', class: 'flex-column' },
                 { id: 'answerItem2', text: 'Черный', img: './image/burgerBlack.png', class: 'justify-content-center' }
             ];
 
-            formAnswers.innerHTML = '';
+            elems.modal.answers.innerHTML = '';
             data.forEach(row => {
                 const elem = document.createElement('div');
                 elem.classList.add('answers-item');
                 elem.classList.add('d-flex');
                 elem.classList.add(row.class);
                 elem.innerHTML = `
-                    <input type="radio" id="${row.id}" name="answer" class="d-none">
+                    <input type="${type}" id="${row.id}" name="answer" class="d-none">
                     <label for="${row.id}" class="d-flex flex-column justify-content-between">
                         <img class="answerImg" src="${row.img}" alt="burger">
                         <span>${row.text}</span>
                     </label>
                 `;
-                formAnswers.appendChild(elem);
+                elems.modal.answers.appendChild(elem);
             });
         }
         renderQuestions();
     }
+
+    function applySelector(obj) {
+        function applyFunction(obj, fun, context) {
+            let res = {};
+            Object.keys(obj).forEach(function(key) {
+                res[key] = typeof obj[key] === 'object' ?
+                    applyFunction(obj[key], fun, context) :
+                    fun.call(context, obj[key]);
+            });
+            return res;
+        }
+
+        return applyFunction(obj, document.querySelector, document);
+    }
+
+    init();
 });
