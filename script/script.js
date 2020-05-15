2de2ef39e578c4fbbbacdd0fb1a00ddb5139f979
Sergey Meshchankin <smeshchankin@gmail.com>
2020-05-15 17:03:38 +0300

    Day 1. soluition

--- script/script.js
+++ script/script.js
@@ -17,23 +17,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const playTest = () => {
         const renderQuestions = () => {
             questionTitle.textContent = 'Какого цвета бургер вы хотите?';
+            const type = 'radio';
+            const data = [
+                { id: 'answerItem1', text: 'Стандарт', img: './image/burger.png', class: 'flex-column' },
+                { id: 'answerItem2', text: 'Черный', img: './image/burgerBlack.png', class: 'justify-content-center' }
+            ];
 
-            formAnswers.innerHTML = `
-            <div class="answers-item d-flex flex-column">
-                <input type="radio" id="answerItem1" name="answer" class="d-none">
-                <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="./image/burger.png" alt="burger">
-                    <span>Стандарт</span>
-                </label>
-            </div>
-            <div class="answers-item d-flex justify-content-center">
-                <input type="radio" id="answerItem2" name="answer" class="d-none">
-                <label for="answerItem2" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="./image/burgerBlack.png" alt="burger">
-                    <span>Черный</span>
-                </label>
-            </div>
-            `;
+            formAnswers.innerHTML = '';
+            data.forEach(row => {
+                const elem = document.createElement('div');
+                elem.classList.add('answers-item');
+                elem.classList.add('d-flex');
+                elem.classList.add(row.class);
+                elem.innerHTML = `
+                    <input type="radio" id="${row.id}" name="answer" class="d-none">
+                    <label for="${row.id}" class="d-flex flex-column justify-content-between">
+                        <img class="answerImg" src="${row.img}" alt="burger">
+                        <span>${row.text}</span>
+                    </label>
+                `;
+                formAnswers.appendChild(elem);
+            });
         }
         renderQuestions();
     }
